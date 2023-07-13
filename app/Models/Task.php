c8c675ce3ef944e602f8228706fd7759dbac6faa
huupt-0333 <pham.tien.huu@sun-asterisk.com>
2023-07-13 12:16:37 +0700

    relationship

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,13 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
     use HasFactory;
 
-    protected $fillable=[
+    protected $fillable = [
         'name',
         'description',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
